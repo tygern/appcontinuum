3ac119c4e5f712e73c6b3ce07aec61408a0928ae
barinek <barinek@gmail.com>
2017-01-30 22:06:54 -0700

    Components

---
+++ components/projects/src/test/kotlin/test/barinek/continuum/projects/ProjectControllerTest.kt
@@ -0,0 +1,67 @@
+package test.barinek.continuum.projects
+
+import com.fasterxml.jackson.core.type.TypeReference
+import io.barinek.continuum.TestControllerSupport
+import io.barinek.continuum.TestDataSourceConfig
+import io.barinek.continuum.TestScenarioSupport
+import io.barinek.continuum.jdbcsupport.JdbcTemplate
+import io.barinek.continuum.projects.ProjectController
+import io.barinek.continuum.projects.ProjectDataGateway
+import io.barinek.continuum.projects.ProjectInfo
+import io.barinek.continuum.restsupport.BasicApp
+import org.apache.http.message.BasicNameValuePair
+import org.eclipse.jetty.server.handler.HandlerList
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class ProjectControllerTest : TestControllerSupport() {
+    internal var app: BasicApp = object : BasicApp() {
+
+        override fun getPort() = 8081
+
+        override fun handlerList() = HandlerList().apply {
+            val dataSource = TestDataSourceConfig().dataSource
+            addHandler(ProjectController(mapper, ProjectDataGateway(JdbcTemplate(dataSource))))
+        }
+    }
+
+    @Before
+    fun setUp() {
+        app.start()
+    }
+
+    @After
+    fun tearDown() {
+        app.stop()
+    }
+
+
+    @Test
+    fun testCreate() {
+        TestScenarioSupport().loadTestScenario("jacks-test-scenario")
+
+        val json = "{\"accountId\":1673,\"name\":\"aProject\"}"
+        val response = template.post("http://localhost:8081/projects", json)
+        val actual = mapper.readValue(response, ProjectInfo::class.java)
+
+        assertEquals(1673L, actual.accountId)
+        assertEquals("aProject", actual.name)
+        assertEquals("project info", actual.info)
+    }
+
+    @Test
+    fun testList() {
+        TestScenarioSupport().loadTestScenario("jacks-test-scenario")
+
+        val response = template.get("http://localhost:8081/projects", BasicNameValuePair("accountId", "1673"))
+        val list: List<ProjectInfo> = mapper.readValue(response, object : TypeReference<List<ProjectInfo>>() {})
+        val actual = list.first()
+
+        assertEquals(55432L, actual.id)
+        assertEquals(1673L, actual.accountId)
+        assertEquals("Flagship", actual.name)
+        assertEquals("project info", actual.info)
+    }
+}
